7f2d39c5797cc13fcdc64adef123d1b980ccdd70
tqt1345 <tqt1345@gmail.com>
2023-05-29 21:50:15 +1000

    refactored getProduct to use streams

--- src/main/java/com/mycompany/botanyhub/Product/ProductUtils.java
+++ src/main/java/com/mycompany/botanyhub/Product/ProductUtils.java
@@ -3,11 +3,20 @@ package com.mycompany.botanyhub.Product;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductUtils {
 
-    public static Product getProduct (String inputtedName, List<? extends Product> products) throws Exception {
+
+    public static Product getProduct (String inputtedName, ArrayList<? extends Product> inputtedProductList) throws Exception {
+        return inputtedProductList.stream()
+                .filter(product -> product.getName().equals(inputtedName)).findFirst()
+                .orElseThrow( () -> new Exception(String.format("Error while getting product\n" +
+                        "Could not find product with name: %s", inputtedName)));
+    }
+
+   /* public static Product getProductOld (String inputtedName, List<? extends Product> products) throws Exception {
         for (Product product : products) {
             if (product.getName().equals(inputtedName)) {
                 return product;
@@ -15,7 +24,7 @@ public class ProductUtils {
         }
         throw new Exception(String.format("Error while getting product\n" +
                 "Product with name: %s not found inside inputted list", inputtedName));
-    }
+    }*/
 
     public static ObservableList<String> getProductNamesAsObservableList (List<? extends Product> products) {
         ObservableList<String> productNames = FXCollections.observableArrayList();
